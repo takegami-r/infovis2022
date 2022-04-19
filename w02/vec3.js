e3fc322639e2fb542b23ceff5134c85a4c27c523
takegami-r <103481192+takegami-r@users.noreply.github.com>
2022-04-19 13:31:01 +0900

    Update vec3.js

--- w02/vec3.js
+++ w02/vec3.js
@@ -1,56 +1,63 @@
-class Vec3{
-    
-    constructor( x, y, z ){
+class Vec3
+{
+    constructor( x, y, z )
+    {
         this.x = x;
         this.y = y;
         this.z = z;
-        super();
     }
-    
-    add(v){
+
+    add( v )
+    {
         this.x += v.x;
         this.y += v.y;
         this.z += v.z;
         return this;
     }
-    
-    sub(v){
+
+    sub( v )
+    {
         this.x -= v.x;
         this.y -= v.y;
         this.z -= v.z;
         return this;
     }
-    
-    sum(){
+
+    sum()
+    {
         return this.x + this.y + this.z;
     }
-    
-    min(){
-        return Math.min( this.x, this.y, this.z );
-        //const m =  this.x < this.y ? this.x : this.y;
-        //return m < this.z ? m : this.z;
+
+    min()
+    {
+        //return Math.min( this.x, this.y, this.z );
+        const m =  this.x < this.y ? this.x : this.y;
+        return m < this.z ? m : this.z;
     }
-    
-    max(){
-        return Math.max( this.x, this.y, this.z );
-        //const m = this.x > this.y ? this.x : this.y;
-        //return m > this.z ? this.z : m;
+
+    max()
+    {
+        //return Math.max( this.x, this.y, this.z );
+        const m = this.x > this.y ? this.x : this.y;
+        return m > this.z ? this.z : m;
     }
-    
-    mid(){
+
+    mid()
+    {
         return this.sum() - this.min() - this.max();
     }
-    
-    cross(v){
+
+    cross( v )
+    {
         var x = this.x, y = this.y, z = this.z;
         this.x = y * v.z - z * v.y;
         this.y = z * v.x - x * v.z;
         this.z = x * v.y - y * v.x;
         return this;
     }
-    
-    length(){
+
+    length()
+    {
         return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
     }
-    
 }
